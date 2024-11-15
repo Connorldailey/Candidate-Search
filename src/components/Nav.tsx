63528a0d0b2498f64a1819e13109662cd316747c
Connor Dailey <connorldailey@gmail.com>
2024-11-15 11:19:58 -0500

    Add code to display the nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,30 @@
+import { Link, useLocation } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
+
   return (
-    <div>Nav</div>
+    <nav>
+      <ul className='nav'>
+        <li className='nav-item'>
+          <Link
+            to="/"
+            className={`nav-link ${currentPage === '/' ? 'active' : ''}`}
+          >
+            Home
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link
+            to="/SavedCandidates"
+            className={`nav-link ${currentPage === '/SavedCandidates' ? 'active' : ''}`}
+          >
+            Potential Candidates
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
